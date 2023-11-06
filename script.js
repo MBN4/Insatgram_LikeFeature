466caceb80c3d289f140851564e36428cae52943
MBN4 <139562028+MBN4@users.noreply.github.com>
2023-11-06 11:17:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var con = document.querySelector(".container")
+var luv = document.querySelector("i")
+
+con.addEventListener("dblclick", function(){
+    luv.style.transform = 'translate(-50%, -50%) scale(1)';
+    luv.style.opacity = 0.7;
+
+    setTimeout(function(){
+        luv.style.opacity = 0;
+    },1000)
+    setTimeout(function(){
+        luv.style.transform = 'translate(-50%, -50%) scale(0)';
+    },1500)
+})
+
